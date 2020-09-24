a1404ef623698e88253f29671addb3d505d1da86
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-24 03:10:17 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
 def starts_with_a_vowel?(word)
-
-end
+  if word == word.match(/\A[aeiou]/) #only returns first letter....
+    true
+  else
+    false 
+  end
+end 
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w+un|\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/[a-zA-Z]{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text == text.match(/^[A-Z]|\W$/)
+    true 
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(//)
 end
