4d2fa4bd68fb10eecfeee503f712cf47353a21d4
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-24 03:54:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z]\w+\b.*\W$/)
+  if text.match(/^[A-Z]\w+\b.*[.,!?]$/)
     true 
   else
     false
